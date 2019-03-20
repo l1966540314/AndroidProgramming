fa31237c19dd9556dc4ba1e6e1921bb08cc760cc
刘昊 <liuhao@liuhaodeMacBook-Pro.local>
2019-03-20 21:26:12 +0800

    第六章AndroidSDK版本与兼容

--- 我的代码/Base2019-02-25/app/src/main/java/com/study/liuhao/geoquiz/CheatActivity.java
+++ 我的代码/Base2019-02-25/app/src/main/java/com/study/liuhao/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.study.liuhao.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.util.Log;
@@ -45,6 +49,26 @@ public class CheatActivity extends AppCompatActivity {
                 setAnswerShowResult(true);
             }
         });
+
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+//            int cx = mShowAnswer.getWidth() / 2;
+//            int cy = mShowAnswer.getHeight() / 2;
+//            float radius = mShowAnswer.getWidth();
+//            Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer,cx,cy,radius,0);
+//            anim.addListener(new AnimatorListenerAdapter() {
+//                @Override
+//                public void onAnimationEnd(Animator animation) {
+//                    super.onAnimationEnd(animation);
+//                    mShowAnswer.setVisibility(View.INVISIBLE);
+//                }
+//            });
+//        }else {
+//            mShowAnswer.setVisibility(View.INVISIBLE);
+//        }
+
+
+
+
 
     }
 
